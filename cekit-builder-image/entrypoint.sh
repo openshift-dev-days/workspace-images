9dbb5716e99b4e9f392ad45e33faa78eb8e2dcc9
cgruver <cgruver@redhat.com>
2026-06-26 13:59:43 +0000

    Create instance of the cekit builder image that does not depend on user namespace support
    Signed-off-by: cgruver <cgruver@redhat.com>

--- cekit-builder-image/entrypoint.sh
+++ cekit-builder-image/entrypoint.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
-# Create Home directory
 if [ ! -d "${HOME}" ]
 then
   mkdir -p "${HOME}"
 fi
 
-# Create Podman configuration
-if [ ! -d "${HOME}/.config/containers" ]; then
+if [ ! -d "${HOME}/.config/containers" ]
+then
   mkdir -p ${HOME}/.config/containers
-  if [ -c "/dev/fuse" ] && [ -f "/usr/bin/fuse-overlayfs" ]; then
-    (echo '[storage]';echo 'driver = "overlay"';echo 'graphroot = "/tmp/graphroot"';echo '[storage.options.overlay]';echo 'mount_program = "/usr/bin/fuse-overlayfs"') > ${HOME}/.config/containers/storage.conf
-  else
-    (echo '[storage]';echo 'driver = "vfs"') > "${HOME}"/.config/containers/storage.conf
+  (echo '[storage]';echo 'driver = "overlay"';echo 'graphroot = "/tmp/graphroot"';echo '[storage.options.overlay]';echo 'mount_program = "/usr/bin/fuse-overlayfs"') > ${HOME}/.config/containers/storage.conf
+fi
+
+if ! whoami &> /dev/null
+then
+  if [ -w /etc/passwd ]
+  then
+    echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
+    echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
   fi
 fi
+USER=$(whoami)
+START_ID=$(( $(id -u)+1 ))
+echo "${USER}:${START_ID}:2147483646" > /etc/subuid
+echo "${USER}:${START_ID}:2147483646" > /etc/subgid
 
 exec "$@"
