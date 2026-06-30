6f8e3d38a5fe4d032dac3c72494ea8e5070e5b94
cgruver <cgruver@redhat.com>
2026-06-30 16:56:41 +0000

    update path for kilo config file
    Signed-off-by: cgruver <cgruver@redhat.com>

--- modules/kilocode/install.sh
+++ modules/kilocode/install.sh
@@ -6,8 +6,8 @@ cat << EOF >> /workspace-init.sh
 if [ -f /globalconfig/kilo.jsonc ]
 then
   mkdir -p ${HOME}/.config/kilo
-  cp /globalconfig/kilo.jsonc /etc/opencode/kilo.jsonc
-  sed -i "s|_LLM_BASE_URL_|\${LLM_BASE_URL}|g" /etc/opencode/kilo.jsonc
-  sed -i "s|_LLM_API_KEY_|\${LLM_API_KEY}|g" /etc/opencode/kilo.jsonc
+  cp /globalconfig/kilo.jsonc ${HOME}/.config/kilo/kilo.jsonc
+  sed -i "s|_LLM_BASE_URL_|\${LLM_BASE_URL}|g" ${HOME}/.config/kilo/kilo.jsonc
+  sed -i "s|_LLM_API_KEY_|\${LLM_API_KEY}|g" ${HOME}/.config/kilo/kilo.jsonc
 fi
 EOF
